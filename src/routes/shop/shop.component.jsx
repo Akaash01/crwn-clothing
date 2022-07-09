dc642cb7090044ccb61f2efce49e8b476500980f
Akaash01 <54339987+Akaash01@users.noreply.github.com>
2022-07-09 16:03:38 +0530

    redux change

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -5,15 +5,15 @@ import { Routes, Route } from 'react-router-dom';
 import CategoriesPreview from '../categories-preview/CategoriesPreview.component';
 import Category from '../category/Category.component';
 import { getCategoriesAndDocuments } from '../../utilities/firebase/firebase.util';
-import { setCategoriesMap } from '../../store/categories/category.action';
+import { setCategories } from '../../store/categories/category.action';
 const Shop = () => {
   const dispatch = useDispatch();
 
   useEffect(() => {
     const getCategoriesMap = async () => {
-      const categoryMap = await getCategoriesAndDocuments();
-      console.log(categoryMap);
-      dispatch(setCategoriesMap(categoryMap));
+      const categoryArray = await getCategoriesAndDocuments();
+      console.log(categoryArray);
+      dispatch(setCategories(categoryArray));
     };
     getCategoriesMap();
   }, []);
